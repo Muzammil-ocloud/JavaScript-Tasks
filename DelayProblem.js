7d20f2b703aa009345552cb4b27069e69bb4b0a3
Muhammad Muzammil <165777442+Muzammil-ocloud@users.noreply.github.com>
2024-05-06 18:40:21 +0500

    Update DelayProblem.js

--- DelayProblem.js
+++ DelayProblem.js
@@ -1,6 +1,19 @@
-delay()
-async function delay() {
-    console.log(5)
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    console.log(6)
+const promise1 = () => new Promise(resolve => setTimeout(() => resolve(1), 1000));
+const promise2 = () => new Promise(resolve => setTimeout(() => resolve(2), 500));
+const promise3 = () => new Promise(resolve => setTimeout(() => resolve(3), 800));
+const promise4 = () => new Promise(resolve => setTimeout(() => resolve(4), 300));
+const promiseFunctions = [promise1, promise2, promise3, promise4];
+const delay = 1000;
+delayedPromiseChain(promiseFunctions, delay);
+
+
+async function delayedPromiseChain(promiseFunctions, delay) {
+    for await (let promise of promiseFunctions) {
+        let data = await promise()
+        console.log(data)
+        await delay_func(delay);
+    }
+}
+async function delay_func(d) {
+    await new Promise(resolve => setTimeout(resolve, d));
 }
